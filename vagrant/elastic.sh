211397c8bee66214f0819f05db2575a20eb99b7c
ReenExe <ipourri@gmail.com>
2015-11-12 16:44:29 +0200

    install progress

---
+++ vagrant/elastic.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install openjdk-7-jre
+
+wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.2.deb
+sudo dpkg -i elasticsearch-1.7.2.deb
