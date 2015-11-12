b07849d01f820a1904ce46125741e519c3976b1a
ReenExe <ipourri@gmail.com>
2015-11-12 21:20:36 +0200

    blank test

---
+++ app/src/AppBundle/Tests/Elastic/ConnectTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppBundle\Tests\Elastic;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class ConnectTest extends KernelTestCase
+{
+    public function test()
+    {
+
+    }
+}
