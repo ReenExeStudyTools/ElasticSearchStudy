76e3a3350844b24ad98ae1074419c8f5d479c28b
ReenExe <ipourri@gmail.com>
2015-11-12 21:58:21 +0200

    assert connect

--- app/src/AppBundle/Tests/Elastic/ConnectTest.php
+++ app/src/AppBundle/Tests/Elastic/ConnectTest.php
@@ -9,6 +9,12 @@ class ConnectTest extends KernelTestCase
     public function test()
     {
         static::bootKernel();
-        static::$kernel->getContainer()->get('fos_elastica.provider_registry');
+        /* @var $client \FOS\ElasticaBundle\Elastica\Client */
+        $client = static::$kernel->getContainer()->get('fos_elastica.client.default');
+
+        $serverStatus = $client->getStatus()->getServerStatus();
+
+        /* @var $this \PHPUnit_Framework_TestCase */
+        $this->assertSame($serverStatus['status'], 200);
     }
 }
