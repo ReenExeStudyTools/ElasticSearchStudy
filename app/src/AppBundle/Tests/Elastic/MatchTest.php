cc4bb66d932a8384ec42be4e9cccb0406172d470
ReenExe <ipourri@gmail.com>
2015-12-17 22:03:32 +0200

    match_all

---
+++ app/src/AppBundle/Tests/Elastic/MatchTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\Tests\Elastic;
+
+use Elastica\Document;
+use Elastica\Multi\ResultSet;
+
+class MatchTest extends AbstractElasticTestCase
+{
+    public function test()
+    {
+        $client = $this->getClient();
+
+        $index = $client->getIndex('products');
+
+        $index->delete();
+
+        $type = $index->getType('product');
+
+        $id = 1;
+        $source = [
+            'name' => 'Black Shirt'
+        ];
+
+        $type->addDocument(
+            new Document($id, $source)
+        );
+
+        $index->refresh();
+
+        /* @var $resultSet ResultSet */
+        $resultSet = $type->search([
+            'query' => [
+                'match_all' => []
+            ]
+        ]);
+
+        $response = $resultSet->getResponse()->getData();
+
+        $this->assertSame(
+            $response['hits'],
+            [
+                'total' => 1,
+                'max_score' => 1.0,
+                'hits' => [
+                    [
+                        '_index' => 'products',
+                        '_type' => 'product',
+                        '_id' => (string)$id,
+                        '_score' => 1.0,
+                        '_source' => $source,
+                    ]
+                ]
+            ]
+        );
+    }
+}
