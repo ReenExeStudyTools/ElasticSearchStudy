b69bef320abda38d2d4f4a5a65962770d65126f0
ReenExe <ipourri@gmail.com>
2015-12-28 20:05:42 +0200

    progress

--- app/src/AppBundle/Tests/Elastic/FilterTest.php
+++ app/src/AppBundle/Tests/Elastic/FilterTest.php
@@ -2,9 +2,48 @@
 
 namespace AppBundle\Tests\Elastic;
 
+use Elastica\Document;
+use Elastica\Index;
+
 class FilterTest extends AbstractElasticTestCase
 {
     public function test()
     {
+        $client = $this->getClient();
+
+        $index = $client->getIndex('index');
+
+        $this->clearIndex($index);
+
+        $type = $index->getType('type');
+
+        $data = [
+            1 => [
+                'price' => 10
+            ],
+            2 => [
+                'price' => 20
+            ],
+            3 => [
+                'price' => 20
+            ]
+        ];
+
+        $documents = [];
+
+        foreach ($data as $key => $value) {
+            $documents[] = new Document($key, $value);
+        }
+
+        $type->addDocuments($documents);
+
+        $index->refresh();
+
+        $this->assertSearchResult($index, [], []);
+    }
+
+    private function assertSearchResult(Index $index, array $query, array $expect)
+    {
+
     }
 }
