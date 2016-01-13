a4af6720ed33a874ed8ed08d8d7c1b9a39588d3e
ReenExe <ipourri@gmail.com>
2016-01-13 11:49:16 +0200

    use PHP

--- app/src/AppBundle/Tests/Elastic/RussianMorphologyTest.php
+++ app/src/AppBundle/Tests/Elastic/RussianMorphologyTest.php
@@ -40,4 +40,54 @@ class RussianMorphologyTest extends AbstractElasticTestCase
         $response = $resultSet->getResponse()->getData();
         $this->assertSame($response['hits']['total'], 1);
     }
+
+    public function testAnalyzer()
+    {
+        $client = $this->getClient();
+        $index = $client->getIndex('genders');
+        $this->clearIndex($index);
+        $type = $index->getType('gender');
+
+        $client->request('genders', 'PUT', [
+            'mappings' => [
+                'gender' => [
+                    '_all' => [
+                        'analyzer' => 'russian_morphology'
+                    ],
+                    'properties' => [
+                        'body' => [
+                            'type' => 'string',
+                            'analyzer' => 'russian_morphology'
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+
+        $id = 1;
+        $sources = [
+            ['body' => 'женский наряд'],
+            ['body' => 'женское украшение'],
+            ['body' => 'женская одежда'],
+            ['body' => 'женские перчатки'],
+            ['body' => 'женскую обувь'],
+        ];
+
+        foreach ($sources as $source) {
+            $type->addDocument(
+                new Document($id++, $source)
+            );
+        }
+        $index->refresh();
+
+        /* @var $resultSet ResultSet */
+        $resultSet = $type->search([
+            'query' => [
+                'query_string' => ['query' => 'body:женское']
+            ]
+        ]);
+
+        $response = $resultSet->getResponse()->getData();
+        $this->assertSame($response['hits']['total'], 5);
+    }
 }
